5956b50301c090e5484d58f3c26d68f0bd0b33ca
ittat <ittat@live.com>
2020-11-24 20:57:12 +0800

    Update repo_source.sh

--- b2g/source/repo_source.sh
+++ b2g/source/repo_source.sh
@@ -5,5 +5,6 @@ set -e
         git clone ${b2g_source} -b ${b2g_branch}  --depth=1 
         cd ./B2G
         echo Download ...
-        REPO_INIT_FLAGS="--depth=1" REPO_SYNC_FLAGS=" -j128 --force-sync --current-branch --no-tags --no-clone-bundle --optimized-fetch --prune" ./config.sh ${device_name}
+        #TODO
+        GITREPO=https://github.com/ittat/manifests BRANCH=b2g-gsi REPO_INIT_FLAGS="--depth=1" REPO_SYNC_FLAGS=" -j128 --force-sync --current-branch --no-tags --no-clone-bundle --optimized-fetch --prune" ./config.sh ${device_name}
         df -h
