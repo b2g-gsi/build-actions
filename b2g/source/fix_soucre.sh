8f9e4fa6939020d5e3e73f5a3013363492872f67
ittat <ittat@live.com>
2020-11-24 21:37:26 +0800

    Update fix_soucre.sh

--- b2g/source/fix_soucre.sh
+++ b2g/source/fix_soucre.sh
@@ -14,24 +14,24 @@ set -e
         /usr/bin/sed -i '' '65i\'$'\n\"10\.15\"\,\n' build/soong/cc/config/x86_darwin_host.go
       
         ####gecko split
-        cd ${work}
-        cp B2G/gonk-misc/Android.mk ./Android.mk.gonk.old
+        #cd ${work}
+        #cp B2G/gonk-misc/Android.mk ./Android.mk.gonk.old
         
-        cd ~
-        git clone https://github.com/OnePlus-onyx/build-CI -b b2g
-        sudo chmod +x ~/build-CI/patch-b2g-aosp.sh
-        ~/build-CI/patch-b2g-aosp.sh
+        #cd ~
+        #git clone https://github.com/OnePlus-onyx/build-CI -b b2g
+        #sudo chmod +x ~/build-CI/patch-b2g-aosp.sh
+        #~/build-CI/patch-b2g-aosp.sh
         
-        cd ${work}
-        cp B2G/gonk-misc/Android.mk ./Android.mk.gonk.patch
+        #cd ${work}
+        #cp B2G/gonk-misc/Android.mk ./Android.mk.gonk.patch
         
         ####api-daemon split
-        cd ${work}/B2G/gonk-misc
-        if [ -d "${work}/B2G/gonk-misc/api-daemon" ];then
-          echo api-daemon patch
-          sudo rm -r ${work}/B2G/gonk-misc/api-daemon
-        fi
-        git clone https://github.com/ittat/api-daemon -b ittat-patch-without-api-daemon
+        #cd ${work}/B2G/gonk-misc
+        #if [ -d "${work}/B2G/gonk-misc/api-daemon" ];then
+        #  echo api-daemon patch
+        #  sudo rm -r ${work}/B2G/gonk-misc/api-daemon
+        #fi
+        #git clone https://github.com/ittat/api-daemon -b ittat-patch-without-api-daemon
         
         ####onyx
         #if [ ${device_name} == "onyx" ]; then
