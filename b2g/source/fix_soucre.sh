62b4d1f0165f6b0dbf4599fca032cd760aa8d9a1
ittat <ittat@live.com>
2020-11-26 13:31:44 +0800

    Update fix_soucre.sh

--- b2g/source/fix_soucre.sh
+++ b2g/source/fix_soucre.sh
@@ -11,18 +11,19 @@ set -e
         cd ${work}/B2G
         /usr/bin/sed -i '' '14d'  system/sepolicy/tests/Android.bp
         /usr/bin/sed -i '' '65i\'$'\n\"10\.15\"\,\n' build/soong/cc/config/x86_darwin_host.go
-      
-        ####gecko split
-        cd ${work}
-        cp B2G/gonk-misc/Android.mk ./Android.mk.gonk.old
         
+        ###
         cd ~
         git clone https://github.com/OnePlus-onyx/build-CI -b b2g
         sudo chmod +x ~/build-CI/patch-b2g-aosp.sh
         ~/build-CI/patch-b2g-aosp.sh
         
+        ###
         cd ${work}
-        cp B2G/gonk-misc/Android.mk ./Android.mk.gonk.patch
+        # Create a dummy b2g archive
+        mkdir b2g
+        tar cvjf b2g-dummy.tar.bz2 b2g
+        rm -rf b2g
         
         ####api-daemon split
         cd ${work}/B2G/gonk-misc
