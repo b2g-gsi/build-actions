dba97f689f2e15e31b66b382ee1bbf025fce015c
ittat <ittat@live.com>
2021-02-14 20:30:57 +0800

    Update build_with_gecko.sh

--- b2g/build/build_with_gecko.sh
+++ b2g/build/build_with_gecko.sh
@@ -18,10 +18,8 @@ set -e
         export USE_PREBUILT_B2G=1
         #export OUT_DIR_COMMON_BASE=${out_work}
         export PREFERRED_B2G="${work}/pre-gecko/${gecko_version}"
-        
-                #gtimeout 245m ./build.sh -j16 systemimage
         export SKIP_ABI_CHECKS=true 
-        gtimeout 245m  ./build-gsi.sh gsi_arm64_ab systemimage
+        gtimeout 245m  ./build-gsi.sh ${build_device_tag} systemimage
 
         if [ "$device_name" == "onyx" ]; then
         else
