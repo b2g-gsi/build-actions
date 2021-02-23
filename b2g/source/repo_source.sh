439260b74d59c8c20603cc50c2094a207c5cb675
ittat <ittat@live.com>
2021-02-23 23:41:37 +0800

    Update repo_source.sh

--- b2g/source/repo_source.sh
+++ b2g/source/repo_source.sh
@@ -6,5 +6,5 @@ set -e
         cd ./B2G
         echo Download ...
         #TODO
-        GITREPO=https://github.com/b2g-gsi/manifests BRANCH=full-gsi  REPO_INIT_FLAGS="--depth=1" REPO_SYNC_FLAGS=" -j128 --force-sync --current-branch --no-tags --no-clone-bundle --optimized-fetch --prune" ./config.sh b2g_gsi
+        GITREPO=https://github.com/b2g-gsi/manifests BRANCH=gsi_q  REPO_INIT_FLAGS="--depth=1" REPO_SYNC_FLAGS=" -j128 --force-sync --current-branch --no-tags --no-clone-bundle --optimized-fetch --prune" ./config.sh aosp_arm64
         df -h
