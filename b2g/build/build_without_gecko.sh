7ce1ad6bcf3c796d835e407b6bb10dca277d3753
ittat <ittat@live.com>
2021-03-15 20:34:08 +0800

    Update build_without_gecko.sh

--- b2g/build/build_without_gecko.sh
+++ b2g/build/build_without_gecko.sh
@@ -11,5 +11,6 @@ set -e
         export DISABLE_SOURCES_XML=true
         export PREFERRED_B2G=${work}/b2g-dummy.tar.bz2
         export USE_PREBUILT_B2G=1
-        gtimeout 245m  ./build.sh -j16 WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY=false WITH_DEXPREOPT=true
+        gtimeout ./build.sh -j16 dex2oatd
+        gtimeout 245m  ./build.sh -j16
         df -h
